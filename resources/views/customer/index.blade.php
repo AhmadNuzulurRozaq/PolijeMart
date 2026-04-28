09cd308f77432ae69d84dc3fbc81d9a72cf2c4b4
AhmadNuzulurRozaq <nuzulnazil14@gmail.com>
2026-04-28 08:24:31 +0700

    Peruban index

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -6,5 +6,14 @@
 <h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
 <h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
 <h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
 
 @endsection
